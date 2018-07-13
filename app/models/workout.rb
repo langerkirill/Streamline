8d551378b8a88e96f62acc000676cc1e8d575d08
Kirill Langer <langerkirill@gmail.com>
2018-07-13 19:06:16 -0400

    test2

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -19,7 +19,8 @@ class Workout < ApplicationRecord
 
   belongs_to :user,
     foreign_key: :user_id,
-    class_name: :User
+    class_name: :User,
+    optional: true
   # belongs_to :routes
 
 end
