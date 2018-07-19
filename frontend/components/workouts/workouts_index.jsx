b1c513271a02e3be22315f1859e6a65a422f15b5
Kirill Langer <langerkirill@gmail.com>
2018-07-19 14:33:57 -0400

    sorting the workouts by date

--- frontend/components/workouts/workouts_index.jsx
+++ frontend/components/workouts/workouts_index.jsx
@@ -4,14 +4,34 @@ import WorkoutIndexItem from './workouts_index_item';
 import {fetchWorkouts} from '../../actions/workout_actions';
 
 class WorkoutIndex extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      workouts: []
+    }
+    this.dateSorter = this.dateSorter.bind(this);
+  }
+
+  componentWillReceiveProps (nextProps) {
+    let workouts = nextProps.workouts;
+    this.dateSorter(workouts)
+  }
+
+  dateSorter(workouts) {
+    workouts.sort(function(a,b){
+    var c = new Date(a.date);
+    var d = new Date(b.date);
+    return d-c;
+    });
+    this.setState({workouts: workouts})
+  }
 
   render () {
     let workoutIndex;
-
-    if (Object.values(this.props.workouts).length === 0) {
-      workoutIndex = ""; 
+    if (Object.values(this.state.workouts).length === 0) {
+      workoutIndex = "";
     } else {
-      workoutIndex = this.props.workouts.map(workout => {
+      workoutIndex = this.state.workouts.map(workout => {
         return (<WorkoutIndexItem workout={workout} key={workout.id} />);
       });
     }
