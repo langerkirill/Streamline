976f7a2ca96b6b93ef1847d4e3e5bc2807440559
Kirill Langer <langerkirill@gmail.com>
2018-08-07 18:36:18 -0400

    trying to add a workout display on the training log

--- frontend/components/training/training_log_item.jsx
+++ frontend/components/training/training_log_item.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import WorkoutBall from './workout_ball';
 
 class TrainingLogItem extends React.Component {
 
@@ -7,8 +8,10 @@ class TrainingLogItem extends React.Component {
     this.state = {
       miles: 0,
       duration: 0,
-      elevation: 0
+      elevation: 0,
+      workouts: []
     }
+    this.showWorkout = this.showWorkout.bind(this);
   }
 
   componentWillMount () {
@@ -39,10 +42,25 @@ class TrainingLogItem extends React.Component {
     }
     if (showDate.length > 0) {
       this.parseData(showDate);
+      this.showWorkout(showDate);
     }
   }
 
+  showWorkout(workouts){
+    this.setState({workouts: workouts});
+  }
+
   render() {
+
+    const displayWorkout = () => {
+      if (this.state.workouts.length > 0) {
+        this.state.workouts.map((workout) => {
+          return (<WorkoutBall key={workout.id} workout={workout}/>);
+        });
+      } else {
+        return ("");
+      }
+    }
 
     return (
       <section className="log-container">
@@ -60,6 +78,7 @@ class TrainingLogItem extends React.Component {
             <div className="dotted"></div>
             <div className="dotted"></div>
           </div>
+          {displayWorkout()}
         </div>
       </section>
     );
